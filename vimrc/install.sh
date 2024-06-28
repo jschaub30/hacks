ec39d0f807206fc410bb2aac3e514a111b7a07ab
Jeremy Schaub <schaubjeremy@gmail.com>
2024-06-28 11:24:12 -0600

    Switched from pathogen to pack; added ALE

--- vimrc/install.sh
+++ vimrc/install.sh
@@ -5,30 +5,55 @@ mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/undo
 
 DATESTR=$(date +"%Y-%m-%d_%H:%M:%S")
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if false; then
+	# Install awesome-vimrc
+	if [ ! -e ~/.vim_runtime ]; then
+		git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+		sh ~/.vim_runtime/install_awesome_vimrc.sh
+		# but move auto-pairs
+		mv ~/.vim_runtime/sources_non_forked/auto-pairs ~/.vim_runtime/
+	fi
+fi
+
+if false; then
+	# Install pathogen
+	mkdir -p ~/.vim/autoload ~/.vim/bundle
+	if [ ! -e ~/.vim/autoload/pathogen.vim ]; then
+		curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+	fi
+fi
+
+if false; then
+	# vim-syntastic
+	if [ ! -e ~/.vim/bundle/syntastic ]; then
+		mkdir -p ~/.vim/pack/git-plugins/start
+		git clone --depth 1 https://github.com/vim-syntastic/syntastic.git ~/.vim/pack/git-plugins/start/syntastic
+	fi
+fi
 
 if true; then
-    # Install awesome-vimrc
-    if [ ! -e ~/.vim_runtime ]; then
-        git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-        sh ~/.vim_runtime/install_awesome_vimrc.sh
-        # but move auto-pairs
-        mv ~/.vim_runtime/sources_non_forked/auto-pairs ~/.vim_runtime/
-    fi
+	# ALE
+	mkdir -p ~/.vim/pack/git-plugins/start
+	if [ ! -e ~/.vim/pack/git-plugins/start/ale ]; then
+		git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
+	fi
 fi
 
-# Install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-if [ ! -e ~/.vim/autoload/pathogen.vim ]; then
-    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-fi
-
-# Install vim-syntastic as pathogen bundle
-if [ ! -e ~/.vim/bundle/syntastic ]; then
-    cd ~/.vim/bundle && \
-    git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+if true; then
+	# auto-pairs
+	mkdir -p ~/.vim/pack/git-plugins/start
+	if [ ! -e ~/.vim/pack/git-plugins/start/auto-pairs ]; then
+		git clone --depth 1 https://github.com/jiangmiao/auto-pairs.git ~/.vim/pack/git-plugins/start/auto-pairs
+	fi
 fi
 
 touch "$HOME/.vimrc"
 cp "$HOME/.vimrc" "$HOME/.vimrc.bak.$DATESTR"
 cp "$DIR/vimrc" ~/.vimrc
+
+mkdir -p ~/.config/ruff
+cp "$DIR"/ruff.toml ~/.config/ruff/
+
+echo "Be sure to pip install 'ruff', 'isort', and 'pyright'"
