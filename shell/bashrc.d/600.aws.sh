f7dc8349331df78bcfd7e394a7de29db759f0bea
Jeremy Schaub <schaubjeremy@gmail.com>
2024-12-11 11:34:07 -0700

    sign into 1password

--- shell/bashrc.d/600.aws.sh
+++ shell/bashrc.d/600.aws.sh
@@ -1,4 +1,5 @@
 # read AWS credentials using the 1password (op) tool
+eval $(op signin)
 export AWS_ACCESS_KEY_ID=$(op item get "AWS User" --fields label="access key")
 export AWS_SECRET_ACCESS_KEY=$(op item get "AWS User" --fields label="secret key" --reveal)
 export AWS_REGION="us-west-2"
