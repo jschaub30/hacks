04dc3d6374bf2f9460e89c420fe9c5cb7e15ac74
Jeremy Schaub <jeremy.schaub@octanner.com>
2025-01-13 14:41:23 -0700

    added kubectl aliases

--- shell/bashrc.d/900.aliases.sh
+++ shell/bashrc.d/900.aliases.sh
@@ -1,9 +1,26 @@
 alias ll="ls -lha"
-alias kc="kubectl"
-alias ca="conda activate"
-alias cda="conda deactivate"
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias encrypt="gpg -a -c --cipher-algo AES256"
+
+# source: https://krlng.github.io/k8s_shortcuts/
+alias k='kubectl'
+alias kgp='kubectl get pods'
+alias kgs='kubectl get service'
+alias kd='kubectl describe'
+alias kdel='kubectl delete'
+alias klog='kubectl logs'
+alias klf='kubectl logs -f'
+alias kaf='kubectl apply -f'
+
+# list available contexts
+alias kcontext="kubectl config get-contexts"
+# switch between different kubernetes contexts
+alias kc="kubectl config use-context"
+# set default namespace in current context
+
+kns() {
+  kubectl config set-context "$(kubectl config current-context)" --namespace="${1}"
+}
